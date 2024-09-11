92fb1449596d000b5b7ed3d422eb00b685b0d1c9
Ivan Smirnov <smirn.iL2att@gmail.com>
2024-09-11 17:03:05 +0300

    crud

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 type requestBody struct {
 	Message string `json:"message"`
+	Id      string `json:"id"`
 }
 
 var message string
@@ -35,6 +36,36 @@ func GetMessage(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(message)
 }
 
+func PatchMessage(w http.ResponseWriter, r *http.Request) {
+	var id string
+	var new_message string
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	var reqBody requestBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		fmt.Println(err)
+	}
+	new_message = reqBody.Message
+	id = reqBody.Id
+	db.DB.Model(&orm.Message{}).Where("id = ?", id).Update("text", new_message)
+}
+
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	var id string
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	var reqBody requestBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		fmt.Println(err)
+	}
+	id = reqBody.Id
+	db.DB.Where("id = ?", id).Delete(&orm.Message{})
+}
+
 func main() {
 	db.InitDB()
 	err := db.DB.AutoMigrate(&orm.Message{})
@@ -46,6 +77,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/post", PostMessage).Methods("POST")
 	router.HandleFunc("/get", GetMessage).Methods("GET")
+	router.HandleFunc("/patch", PatchMessage).Methods("PATCH")
+	router.HandleFunc("/delete", DeleteMessage).Methods("DELETE")
 
 	err = http.ListenAndServe(":8080", router)
 	if err != nil {
